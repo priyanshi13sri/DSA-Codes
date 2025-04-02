a0170ec4b80bd6e43b252548d8162a2e92136357
priyanshi13sri <priyanshi.main@gmail.com>
2025-04-02 23:32:24 +0530

    MAXIMUM SUBARRAY

---
+++ maximumsubarray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    
+    vector<int> nums;
+    int currSum=0, maxSum=INT_MIN;
+    nums={-2,1,-3,4,-1,2,1,-5,4};
+    for(int val:nums){
+        currSum=currSum+val;
+        maxSum=max(currSum,maxSum);
+        if(currSum<0){
+            currSum=0;
+        }
+    }
+    cout<<maxSum;
+    return 0;
+}
