07f453f0c134dcd04c0df204403923d272388dda
priyanshi13sri <priyanshi.main@gmail.com>
2025-04-02 22:37:06 +0530

    vector in cpp

---
+++ Shifttingletter.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    String S="abc";
+    int arr[3]={3,5,7};
+    for(int i=0;i<=2;i++){
+    
+}   
+    return 0;
+}
--- vector.cpp
+++ vector.cpp
@@ -1,8 +1,38 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
     
-    
-    return 0;
+    vector<int> vec1;
+   // vector<int> vec={1,2,3};
+   // vector<int> vec(4,0);
+  vector<char> vec={'a','b','c','d','e'};
+  cout<<"size ="<< vec.size() <<endl;
+  vec.push_back('g');
+  cout<<"after push back size = "<< vec.size()<<endl;
+  vec.pop_back();
+    cout<< vec.front() <<endl;
+    cout<< vec.back() <<endl; 
+     cout<<vec.at(1)<<endl;
+     vec.push_back('r');
+     vec.push_back('t');
+     cout<< vec.capacity()<<endl; 
+     //for(char val:vec){ //for each loop
+      //  cout<<val<<endl;
+    //}
+   // cout<<vec[0]<<endl;
+   //  cout<<vec[1]<<endl;
+   //  cout<<vec[2]<<endl;
+   //  cout<<vec[3]<<endl; 
+     return 0;
 }
+
+/*  VECTOR FUNCTIONS  ---->
+                *size
+                *push_back
+                *pop_back
+                *front
+                *back
+                *at
+*/
