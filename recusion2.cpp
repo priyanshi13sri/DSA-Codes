7f1047128e8818bb17fdc81ef0e83c4c1ad0dce3
priyanshi13sri <priyanshi.main@gmail.com>
2025-04-06 00:22:56 +0530

    Print N to 1 using recursion

---
+++ recusion2.cpp
@@ -0,0 +1,23 @@
+//Print from N to 1.
+
+#include<iostream>
+using namespace std;
+
+void f(int n, int i){
+    if(n<i)
+    return;
+    f(n,i+1);
+    cout<<i<<endl;
+    
+ 
+  
+
+}
+int main(){
+
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    f(n,1);
+    return 0;
+}
