234b86c48f992fa70cbc04ba6813b070289fa9e6
priyanshi13sri <priyanshi.main@gmail.com>
2025-06-15 13:42:35 +0530

    bubble sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 void BubbleSort(int arr[],int n){
+    bool swapped = false;
     for(int i=1;i<n;i++){
         for(int j=0;j<n-i;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped = true;
             }
         }
+        if(swapped == false)\
+        break;
     }
 }
 int main(){
